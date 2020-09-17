dd4731a18d8e776fd75bbe5b59fecd5cd07388e0
tegar <tegarcahyosetiaji@gmail.com>
2020-09-17 22:26:58 +0700

    routing-modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:HomeComponent,
+  },
+  {
+    path:'dashboard',
+    component:HomeComponent,
+  },
+  {
+    path:'dashboard/detail/:id',
+    component:DetailComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
